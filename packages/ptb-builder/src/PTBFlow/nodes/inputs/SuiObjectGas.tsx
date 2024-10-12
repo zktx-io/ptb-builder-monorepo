7d96cee60bdf98af98dfa03cc491e1d79a2b13d6
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-10-12 17:23:23 +0900

    Update SuiObjectGas.tsx

--- packages/ptb-builder/src/PTBFlow/nodes/inputs/SuiObjectGas.tsx
+++ packages/ptb-builder/src/PTBFlow/nodes/inputs/SuiObjectGas.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { useReactFlow } from '@xyflow/react';
 
 import { type NodeProp } from '..';
 import { PtbHandle } from '../handles';
 import { FormStyle, LabelStyle, NodeStyles } from '../styles';
 
 export const SuiObjectGas = ({ id, data }: NodeProp) => {
+  const { setNodes } = useReactFlow();
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === id) {
+          return {
+            ...node,
+            data: { ...node.data, value: 'gasObject' },
+          };
+        }
+        return node;
+      }),
+    );
+  }, [id, setNodes]);
   return (
     <div className={NodeStyles.object}>
       <div className={FormStyle}>
