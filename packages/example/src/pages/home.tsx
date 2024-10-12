8efbbc52d4ebb540c32242defb369a16d16fea2c
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-10-12 21:29:13 +0900

    Update home.tsx

--- packages/example/src/pages/home.tsx
+++ packages/example/src/pages/home.tsx
@@ -8,76 +8,29 @@ export const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        backgroundColor: 'black',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        position: 'relative',
-      }}
-    >
-      <img
-        style={{
-          height: '128px',
-          width: '128px',
-        }}
-        src="/logo-sui.svg"
-      />
-      <h1
-        style={{
-          color: 'white',
-          fontSize: '34px',
-          marginTop: '12px',
-          marginBottom: '24px',
-        }}
-      >
-        PTB Builder
-      </h1>
+    <div className="container">
+      <img src="/logo-sui.svg" alt="sui" className="logo" />
+      <h1 className="title">PTB Builder</h1>
       <ConnectButton />
-      <div style={{ marginTop: '12px' }}>
+      <div className="button-container">
         {connectionStatus === 'connected' && (
-          <div style={{ color: 'white' }}>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                marginTop: '12px',
-              }}
+          <>
+            <button
+              className="action-button"
+              onClick={() => navigate('/editor')}
             >
-              <button
-                onClick={() => {
-                  navigate('/editor');
-                }}
-              >
-                {'editor >>'}
-              </button>
-              <button
-                onClick={() => {
-                  navigate('/viewer?tx=');
-                }}
-              >
-                {'viewer >>'}
-              </button>
-            </div>
-          </div>
+              Editor
+            </button>
+            <button
+              className="action-button"
+              onClick={() => navigate('/viewer?tx=')}
+            >
+              Viewer
+            </button>
+          </>
         )}
       </div>
-      <div
-        style={{
-          position: 'absolute',
-          bottom: '10px',
-          color: 'white',
-          textAlign: 'center',
-        }}
-      >
-        Developed by zktx.io
-      </div>
+      <div className="footer">Developed by zktx.io</div>
     </div>
   );
 };
