eddf7d93b6ab96902b2a1a0e4ab0dd92bc6a2610
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-11-22 23:04:06 +0900

    update package

--- packages/ptb-builder/rollup.config.js
+++ packages/ptb-builder/rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import postcss from 'rollup-plugin-postcss';
-import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
