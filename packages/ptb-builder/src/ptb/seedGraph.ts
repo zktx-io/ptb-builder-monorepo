c2eb7092cf8ded2bc7d40583a1d4f961b84ef84f
Lim Jet <57783762+daoauth@users.noreply.github.com>
2025-09-01 05:28:45 +0900

    Update seedGraph.ts

--- packages/ptb-builder/src/ptb/seedGraph.ts
+++ packages/ptb-builder/src/ptb/seedGraph.ts
@@ -1,18 +1,21 @@
 import { PTBGraph } from './graph/types';
 import { PORTS } from './portTemplates';
 
+export const START_ID = '@start';
+export const END_ID = '@end';
+
 export function seedDefaultGraph(): PTBGraph {
   return {
     nodes: [
       {
-        id: '@start',
+        id: START_ID,
         kind: 'Start',
         label: 'Start',
         position: { x: 160, y: 300 },
         ports: PORTS.start(), // [{ id:'next', direction:'out', role:'flow' }]
       },
       {
-        id: '@end',
+        id: END_ID,
         kind: 'End',
         label: 'End',
         position: { x: 640, y: 300 },
